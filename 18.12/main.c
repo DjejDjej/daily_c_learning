77ce11b8c0b5625c239109ba793c130791f71e37
Your Name <you@example.com>
2025-12-19 01:35:01 +0100

    prompt

--- 18.12/main.c
+++ 18.12/main.c
@@ -13,17 +13,18 @@ enum ret_code {
 
 int read_line(char *buffer, size_t buffer_length) {
 
-  if(fgets(buffer, buffer_length, stdin) == NULL) { // Fgets fails for 2 reasons
+  // Fgets fails for 2 reasons
+  if (fgets(buffer, buffer_length, stdin) == NULL) {
 
-  if (feof(stdin))
-    return EndOfFile; /// EOF
+    if (feof(stdin))
+      return EndOfFile; /// EOF
 
-  if (ferror(stdin))
-    return FERR; // Broken IO
+    if (ferror(stdin))
+      return FERR; // Broken IO
   }
-  
+
   size_t str_buf_len = strlen(buffer);
-  
+
   for (size_t i = 0; i < str_buf_len; i++) {
 
     if (buffer[i] == '\n') { // new line
@@ -38,7 +39,8 @@ int read_line(char *buffer, size_t buffer_length) {
 int main(int argv, char **argc) {
 
   char *res = malloc(BUF_SIZE);
-  if(!res) return 1; // Malloc failed
+  if (!res)
+    return 1; // Malloc failed
   int code = read_line(res, BUF_SIZE);
   if (code > 0) {
     printf("%d", code);
