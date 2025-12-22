eb881506ab53e7f18cbefcc389eb1b56f7bcacbe
DjejDjej <j.janik1102@gmail.com>
2025-12-22 18:09:21 +0100

    21.12 - finish

--- 21.12/main.c
+++ 21.12/main.c
@@ -3,10 +3,7 @@
 #include <string.h>
 
 #define LINE_DEF_SIZE 10
-#define ALL_CHARS_COUNT_DEF 100
-
-//TODO FIX MALLOC OF **result 
-//TOOD PRIDAT ROLLBACK
+#define LINE_COUNT_DEF 10
 
 enum status_codes {
   IN_PROGRESS = -1,
@@ -14,6 +11,7 @@ enum status_codes {
   REALLOC_FAILED = 1,
   FILE_NULL = 2,
   MALLOC_FAILED = 3,
+  EOF_ON_START = 4,
 
 };
 
@@ -25,12 +23,17 @@ struct result_lines {
 
 int freeAll(struct result_lines l) {
 
-  for (size_t i = 0; i < l.count; i++) {
-
-    printf("%s\n", l.lines[i]);
-    free(l.lines[i]);
-  }
+  for (size_t i = 0; i < l.count; i++) { free(l.lines[i]); }
   free(l.lines);
+  return 0;
+}
+
+int cleanUP(char **result, size_t count, char *buff) {
+
+  for (size_t i = 0; i < count; i++) free(result[i]);
+
+  free(result);
+  free(buff);
   return 0;
 }
 
@@ -56,62 +59,74 @@ int safe_realloc_Ar(char ***buff, size_t size) {
   }
 }
 
-
 struct result_lines readlines(FILE *f) {
+
   struct result_lines r = {-1, NULL, 0};
+  if (f == NULL) {
+    r.status_code = FILE_NULL;
+    return r;
+  }
+
   size_t buff_size = LINE_DEF_SIZE;
-  size_t all_chars_alloc = ALL_CHARS_COUNT_DEF;
-  size_t all_chars_count = 0;
+  size_t lines_capacity = LINE_COUNT_DEF;
 
   char *buff = malloc(buff_size);
-  char **result = malloc(all_chars_alloc * sizeof(char * ));
+  char **result = malloc(lines_capacity * sizeof(char *));
 
   if (buff == NULL || result == NULL) {
     r.status_code = MALLOC_FAILED;
+    free(buff);
+    free(result);
     return r;
   }
 
   int ch;
   int i = 0;
   int count = 0;
-  size_t psize = sizeof(char * );
-  while ((ch = fgetc(f)) != EOF) {
-    if (buff_size + 1 <= i) {
+  ch = fgetc(f);
+  if (ch == EOF) {
+    r.status_code = EOF_ON_START;
+    free(result);
+    return r;
+  }
+  while (1) {
+
+    if (i + 1 >= buff_size) {
       buff_size *= 2;
       if (safe_realloc(&buff, buff_size) == REALLOC_FAILED) {
         r.status_code = REALLOC_FAILED;
+        cleanUP(result, count, buff);
         return r;
       }
     }
 
-    if (all_chars_alloc <= all_chars_count) {
-      all_chars_alloc *= 2;
-      if (safe_realloc_Ar(&result, all_chars_alloc) == REALLOC_FAILED) {
+    if (count >= lines_capacity) {
+      lines_capacity *= 2;
+      if (safe_realloc_Ar(&result, lines_capacity) == REALLOC_FAILED) {
         r.status_code = REALLOC_FAILED;
+        cleanUP(result, count, buff);
         return r;
       }
     }
-    if (ch == '\n' || ch == '\0') {
+    if (ch == '\n' || ch == EOF) {
       buff[i] = '\0';
       result[count] = malloc(buff_size);
-      if(result[count] == NULL) {
-       r.status_code = MALLOC_FAILED;
-	return r;
+      if (result[count] == NULL) {
+        r.status_code = MALLOC_FAILED;
+        cleanUP(result, count, buff);
+        return r;
       }
-      strncpy(result[count++], buff, buff_size);
+      strcpy(result[count++], buff);
       i = 0;
       buff_size = LINE_DEF_SIZE;
-      free(buff);
-      buff = malloc(buff_size);
 
     } else {
-
       buff[i++] = ch;
-      all_chars_count+=psize;
     }
+    if ((ch = fgetc(f)) == EOF) { break; }
   }
-  free(buff);
 
+  free(buff);
   r.lines = result;
   r.count = count;
   r.status_code = OK;
@@ -122,8 +137,9 @@ int main() {
 
   FILE *f = fopen("file", "r");
   struct result_lines l = readlines(f);
+  if (l.status_code != OK) { return 1; }
 
-  printf("%lu\n", l.count);
+  for (size_t i = 0; i < l.count; i++) { printf("%s", l.lines[i]); }
 
   freeAll(l);
   return 0;
